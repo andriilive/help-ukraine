d8602ab932161d8c4466c4bc7e04ac9fadec96db
Andrii Iv. (@digitalandyeu) <hi@digitalandy.eu>
2023-03-01 22:35:59 +0100

    cleanUp : Before nostra

--- next-export.config.js
+++
@@ -1,8 +0,0 @@
-// /** @type {import('next').NextConfig} */
-// module.exports = {
-// 	exportPathMap: async function (defaultPathMap, _ctx) {
-// 		process.env.ANALYZE === 'true' && console.log({ defaultPathMap, _ctx })
-// 		/** @type {import('next/dist/server/config-shared').ExportPathMap } webConfig */
-// 		return defaultPathMap
-// 	},
-// }
--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: { unoptimized: true }, // Disable for static export
-	reactStrictMode: true,
-	swcMinify: true,
-	compiler: {
-		emotion: true,
-	},
 }
 
 module.exports = nextConfig
